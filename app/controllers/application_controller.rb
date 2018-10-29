ac291c2ad86c27b51363c4d77648b4b085b3511b
Kiran Baghele <kiran@synup.com>
2018-10-29 22:06:19 +0530

    authenticate controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user
   include SessionsHelper
+
+  def authenticate_user
+    unless logged_in?
+      redirect_to root_url
+    end
+  end
 end
